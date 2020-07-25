a5a253693f5c8e83122a2df315c863857c6e83f3
Lareta <lareta064@gmail.com>
2020-07-25 15:14:21 +0300

    ста

--- src/js/main.js
+++ src/js/main.js
@@ -86,6 +86,15 @@ $(document).ready(function () {
 	window.addEventListener('resize', function () {
 		hideMobileMenu();
 
-	})
+	});
+	//аккордеон развернуть стрелку
+	$('.collapsable').on('show.bs.collapse', function () {
+		let tabIcon = $("#" + $(this).attr("aria-labelledby")).find(".arrow");
+		tabIcon.addClass("rotate");
+	});
+	$('.collapsable').on('hide.bs.collapse', function () {
+		let tabIcon = $("#" + $(this).attr("aria-labelledby")).find(".arrow");
+		tabIcon.removeClass("rotate");
+	});
 
 })
